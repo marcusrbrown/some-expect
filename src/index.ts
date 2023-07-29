5ac8c8ba51f0529031e9b5d501269e4e3c691ffa
Marcus R. Brown <contact@marcusrbrown.com>
2023-07-28 19:14:30 -0500

    feat: refresh ESLint config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { chaiExpect } = require('./chai');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { jestExpect } = require('./jest');
 
 module.exports = function expect() {};
